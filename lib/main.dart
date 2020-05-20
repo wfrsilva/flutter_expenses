acf9ca07cd1bd0f9f9b4ed535a12ba34285c63a1
Wendel Silva <wfrsilva@gmail.com>
2020-05-19 21:27:56 -0300

    Desafio Estilo Título & Data - Resposta 16/54
    Desafio Estilo Título & Data - Resposta 16/54
    
    https://www.cod3r.com.br/courses/take/aprenda-flutter-dart-e-construa-apps-ios-e-android/lessons/10518956-desafio-estilo-titulo-data-resposta

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyHomePage extends StatelessWidget {
       value: 310.70,
       date: DateTime.now(),
     ),
-     Transaction(
+    Transaction(
       id: 't2',
       title: 'Conta de Luz',
       value: 211.30,
@@ -43,38 +43,54 @@ class MyHomePage extends StatelessWidget {
               elevation: 5,
             ),
           ),
-          Column(children: _transactions.map((tr){
-            return Card(
-              child: Row(
-                children: <Widget>[
-                  Container(
-                    child: Container(
-                      margin: EdgeInsets.symmetric(
-                        horizontal: 15,
-                        vertical: 10
-                      ),
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Colors.black,
-                          width: 2,
+          Column(
+            children: _transactions.map((tr) {
+              return Card(
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      child: Container(
+                        margin:
+                            EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.purple,
+                            width: 2,
+                          ),
+                        ),
+                        padding: EdgeInsets.all(10),
+                        child: Text(
+                          tr.value.toString(),
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: Colors.purple,
+                          ),
                         ),
                       ),
-                      padding: EdgeInsets.all(10),
-                      child: Text(
-                        tr.value.toString()
-                      ),
                     ),
-                  ),
-                  Column(
-                    children: <Widget>[
-                      Text(tr.title),
-                      Text(tr.date.toString()),
-                    ],
-                  ),
-                ],
-              ),
-            );
-          }).toList(),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          tr.title,
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          tr.date.toString(),
+                          style: TextStyle(
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              );
+            }).toList(),
           )
         ],
       ),
