ed715449916fd1562231ba0a834e3cdcccf4af2e
Wendel Silva <wfrsilva@gmail.com>
2020-06-04 17:49:16 -0300

    Objeto Media Query 14/24
    Objeto Media Query 14/24
    
    https://www.cod3r.com.br/courses/take/aprenda-flutter-dart-e-construa-apps-ios-e-android/lessons/10672069-objeto-media-query

--- lib/main.dart
+++ lib/main.dart
@@ -156,8 +156,8 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    bool isLandscape =
-        MediaQuery.of(context).orientation == Orientation.landscape;
+    final mediaQuery = MediaQuery.of(context);
+    bool isLandscape = mediaQuery.orientation == Orientation.landscape;
     final appBar = AppBar(
       title: Text('Despesas Pessoais'),
       actions: <Widget>[
@@ -176,9 +176,9 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ],
     );
-    final avaliableHeight = MediaQuery.of(context).size.height -
+    final avaliableHeight = mediaQuery.size.height -
         appBar.preferredSize.height -
-        MediaQuery.of(context).padding.top;
+        mediaQuery.padding.top;
 
     return Scaffold(
       appBar: appBar,
